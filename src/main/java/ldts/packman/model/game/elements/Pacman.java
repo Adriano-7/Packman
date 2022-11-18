846f0104cea61c492c9c17567561ea6fe99e357f
Félix Martins <100770338+Minipoloalex@users.noreply.github.com>
2022-11-18 17:08:45 +0000

    Added Pacman class and tests for it

---
+++ src/main/java/ldts/packman/model/game/elements/Pacman.java
@@ -0,0 +1,15 @@
+package ldts.packman.model.game.elements;
+
+public class Pacman extends Element {
+    int health;
+    public Pacman(int x, int y) {
+        super(x, y);
+        this.health = 3;
+    }
+    public int getHealth() {
+        return health;
+    }
+    public void decreaseHealth() {
+        this.health--;
+    }
+}
