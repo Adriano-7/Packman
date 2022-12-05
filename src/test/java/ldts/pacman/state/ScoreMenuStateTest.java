6cd60f77fb70457d9ad481639857fcd04cba5591
Félix Martins <100770338+Minipoloalex@users.noreply.github.com>
2022-12-05 11:10:53 +0000

    Added tests for ScoreMenuState
    Also tested simple methods in MenuState

---
+++ src/test/java/ldts/pacman/state/ScoreMenuStateTest.java
@@ -0,0 +1,52 @@
+package ldts.pacman.state;
+
+import ldts.pacman.Game;
+import ldts.pacman.controller.Controller;
+import ldts.pacman.controller.game.ArenaController;
+import ldts.pacman.controller.menu.ScoreMenuController;
+import ldts.pacman.gui.GUI;
+import ldts.pacman.model.game.arena.Arena;
+import ldts.pacman.model.menu.ScoreMenu;
+import ldts.pacman.view.Viewer;
+import ldts.pacman.view.game.ArenaViewer;
+import ldts.pacman.view.menu.ScoreMenuViewer;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.mockito.Mockito.times;
+
+public class ScoreMenuStateTest {
+    public ScoreMenuState scoreMenuState;
+    public ScoreMenu scoreMenu;
+    @BeforeEach
+    public void setUp(){
+        scoreMenu = new ScoreMenu();
+        scoreMenuState = new ScoreMenuState(scoreMenu);
+    }
+    @Test
+    public void getModel(){
+        assertEquals(scoreMenu, scoreMenuState.getModel());
+    }
+    @Test
+    public void getViewer(){
+        assertEquals(ScoreMenuViewer.class, scoreMenuState.getViewer().getClass());
+    }
+    @Test
+    public void getController(){
+        assertEquals(ScoreMenuController.class, scoreMenuState.getController().getClass());
+    }
+    @Test
+    public void step() throws IOException {
+        GUI gui = Mockito.mock(GUI.class);
+
+        scoreMenuState.step(null, gui, 0);
+
+        Mockito.verify(gui, times(1)).getNextOption();
+    }
+}
