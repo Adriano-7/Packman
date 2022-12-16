1cb712b98e27bf4005055db716e7173c8a813aba
Félix Martins <100770338+Minipoloalex@users.noreply.github.com>
2022-12-16 17:48:40 +0000

    Removed unnecessary methods

--- src/main/java/ldts/pacman/model/game/Position.java
+++ src/main/java/ldts/pacman/model/game/Position.java
@@ -7,22 +7,8 @@ public class Position {
     private final int y;
     public Position(int x, int y){this.x=x; this.y=y;}
 
-    public Position getRight(){return new Position(x+1,y);}
-    public Position getLeft(){return new Position(x-1,y);}
-    public Position getDown(){return new Position(x,y+1);}
-    public Position getUp(){return new Position(x,y-1);}
     public int getX(){return this.x;}
     public int getY(){return this.y;}
-    public Position getRandomNeighbour(){
-        int n=(int) (Math.random()*4);
-        return switch (n) {
-            case 0 -> getDown();
-            case 1 -> getUp();
-            case 2 -> getLeft();
-            case 3 -> getRight();
-            default -> null;
-        };
-    }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -34,7 +20,6 @@ public class Position {
     public int hashCode() {
         return Objects.hash(x, y);
     }
-
     public Position plus(Position other) {
         return new Position(this.x + other.x, this.y + other.y);
     }
--- src/test/java/ldts/pacman/model/game/PositionTest.java
+++ src/test/java/ldts/pacman/model/game/PositionTest.java
@@ -14,30 +14,6 @@ public class PositionTest {
     }
 
     @Test
-    public void testGetLeft() {
-        Position left = position.getLeft();
-        assertEquals(4, left.getX());
-        assertEquals(-1, left.getY());
-    }
-    @Test
-    public void testGetRight() {
-        Position left = position.getRight();
-        assertEquals(6, left.getX());
-        assertEquals(-1, left.getY());
-    }
-    @Test
-    public void testGetUp() {
-        Position left = position.getUp();
-        assertEquals(5, left.getX());
-        assertEquals(-2, left.getY());
-    }
-    @Test
-    public void testGetDown() {
-        Position left = position.getDown();
-        assertEquals(5, left.getX());
-        assertEquals(0, left.getY());
-    }
-    @Test
     public void testGetX() {
         int x = position.getX();
         assertEquals(5, x);
@@ -48,15 +24,9 @@ public class PositionTest {
         assertEquals(-1, y);
     }
     @Test
-    public void testGetRandomNeighbour(){
-        Position initial_pos= position;
-        Position final_pos=position.getRandomNeighbour();
-        assertTrue((initial_pos.getDown().equals(final_pos) || initial_pos.getUp().equals(final_pos) || initial_pos.getLeft().equals(final_pos) || initial_pos.getRight().equals(final_pos)));
-    }
-    @Test
     public void testEquals() {
         assertFalse(position.equals(null));
-        assertFalse(position.equals(new String("ABC")));
+        assertFalse(position.equals("ABC"));
         assertTrue(position.equals(position));
         assertTrue(position.equals( new Position(position.getX(), position.getY()) ));
     }
