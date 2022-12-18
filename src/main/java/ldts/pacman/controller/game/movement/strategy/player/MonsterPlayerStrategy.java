05dcd1099c3a4d32280b01dc5d48b477d916047c
Adriano Machado <93844395+Adriano-7@users.noreply.github.com>
2022-12-18 12:27:54 +0000

    Solved bug in the multiplayer

--- src/main/java/ldts/pacman/controller/game/movement/strategy/player/MonsterPlayerStrategy.java
+++ src/main/java/ldts/pacman/controller/game/movement/strategy/player/MonsterPlayerStrategy.java
@@ -14,10 +14,10 @@ public class MonsterPlayerStrategy extends PlayerMovementStrategy {
     }
     private static Map<GUI.OPTION, Position> createOptionToDirectionMap() {
         Map<GUI.OPTION, Position> pacmanOptionToDirection = new HashMap<>();
-        pacmanOptionToDirection.put(GUI.OPTION.UP, new Position(0, -1));
-        pacmanOptionToDirection.put(GUI.OPTION.DOWN, new Position(0, 1));
-        pacmanOptionToDirection.put(GUI.OPTION.LEFT, new Position(-1, 0));
-        pacmanOptionToDirection.put(GUI.OPTION.RIGHT, new Position(1, 0));
+        pacmanOptionToDirection.put(GUI.OPTION.UP2, new Position(0, -1));
+        pacmanOptionToDirection.put(GUI.OPTION.DOWN2, new Position(0, 1));
+        pacmanOptionToDirection.put(GUI.OPTION.LEFT2, new Position(-1, 0));
+        pacmanOptionToDirection.put(GUI.OPTION.RIGHT2, new Position(1, 0));
         return pacmanOptionToDirection;
     }
 }
