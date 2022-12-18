6435e95c82831830b63d906ace8c8b426f01af49
Félix Martins <100770338+Minipoloalex@users.noreply.github.com>
2022-12-18 11:09:00 +0000

    Added simple test for SaveScore

--- src/test/java/ldts/pacman/model/menu/SaveScoreTest.java
+++ src/test/java/ldts/pacman/model/menu/SaveScoreTest.java
@@ -6,7 +6,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SaveScoreTest {
     private SaveScore saveScore;
@@ -20,10 +24,25 @@ public class SaveScoreTest {
     }
     @Test
     public void getters() {
-
         int expectedSizeOfTimeStamp = 5;
 
         assertEquals(100, saveScore.getScore());
         assertEquals(expectedSizeOfTimeStamp, saveScore.getTimeStamp().length());
+    }
+    @Test
+    public void getNumberEntries() {
+        assertEquals(2, saveScore.getNumberEntries());
+    }
+    @Test
+    public void isSelectedSave() throws UnsupportedAudioFileException, LineUnavailableException, IOException {
+        assertTrue(saveScore.isSelectedSave());
+
+        saveScore.next_Op();
+
+        assertFalse(saveScore.isSelectedSave());
+
+        saveScore.next_Op();
+
+        assertTrue(saveScore.isSelectedSave());
     }
 }
