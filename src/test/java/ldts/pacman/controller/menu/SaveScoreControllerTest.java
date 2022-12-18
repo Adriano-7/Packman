efb70a56a4ce0318f86af457fc2940c215abdd3e
Félix Martins <100770338+Minipoloalex@users.noreply.github.com>
2022-12-18 10:53:16 +0000

    Implemented tests for SaveScoreController

--- src/test/java/ldts/pacman/controller/menu/SaveScoreControllerTest.java
+++ src/test/java/ldts/pacman/controller/menu/SaveScoreControllerTest.java
@@ -8,48 +8,83 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.times;
 
 public class SaveScoreControllerTest {
     private Game game;
     private SaveScoreController saveScoreController;
+    private SaveScore saveScore;
     @BeforeEach
     public void setUp() {
-        game = Mockito.mock(Game.class);
-        SaveScore saveScore = Mockito.mock(SaveScore.class);
-        saveScoreController = new SaveScoreController(saveScore);
+        this.game = Mockito.mock(Game.class);
+        this.saveScore = Mockito.mock(SaveScore.class);
+        Mockito.when(saveScore.isSelectedSave()).thenReturn(false);
+        this.saveScoreController = new SaveScoreController(saveScore);
     }
-    /*
-    @Test
-    public void stepNoQuit() {
-        try {
-            saveScoreController.step(game, GUI.OPTION.NONE, 0);
-            saveScoreController.step(game, GUI.OPTION.UP, 0);
-            saveScoreController.step(game, GUI.OPTION.DOWN, 0);
-            saveScoreController.step(game, GUI.OPTION.RIGHT, 0);
-            saveScoreController.step(game, GUI.OPTION.LEFT, 0);
-        }
-        catch (IOException e) {
-            fail();
-        }
+    public void getModel() {
+        assertEquals(saveScore, saveScoreController.getModel());
+    }
 
+
+    @Test
+    public void stepGoUp() throws UnsupportedAudioFileException, LineUnavailableException, IOException {
+        saveScoreController.step(game, List.of(GUI.OPTION.UP), 0);
+        Mockito.verify(saveScore, times(1)).prev_Op();
+        Mockito.verifyNoInteractions(game);
+    }
+    @Test
+    public void stepGoDown() throws UnsupportedAudioFileException, LineUnavailableException, IOException {
+        saveScoreController.step(game, List.of(GUI.OPTION.DOWN), 0);
+
+        Mockito.verify(saveScore, times(1)).next_Op();
         Mockito.verifyNoInteractions(game);
     }
     @Test
     public void stepWithQuit() {
         try {
-            saveScoreController.step(game, GUI.OPTION.SELECT, 0);
-            saveScoreController.step(game, GUI.OPTION.QUIT, 0);
+            saveScoreController.step(game, Arrays.asList(GUI.OPTION.SELECT, GUI.OPTION.QUIT), 0);
         }
-        catch (IOException e) {
+        catch (IOException | UnsupportedAudioFileException | LineUnavailableException e) {
             fail();
         }
 
-        Mockito.verify(game, Mockito.times(2))
+        Mockito.verify(game, times(2))
                 .setState(Mockito.any(MainMenuState.class));
-    }
+        Mockito.verifyNoMoreInteractions(game);
 
-     */
+        Mockito.verify(saveScore, times(1)).isSelectedSave();
+        Mockito.verifyNoMoreInteractions(saveScore);
+    }
+    @Test
+    public void stepNoQuit() {
+        try {
+            List<GUI.OPTION> options = Arrays.asList(GUI.OPTION.UP, GUI.OPTION.DOWN, GUI.OPTION.RIGHT, GUI.OPTION.LEFT);
+
+            saveScoreController.step(game, options, 0);
+        }
+        catch (IOException | UnsupportedAudioFileException | LineUnavailableException e) {
+            fail();
+        }
+        Mockito.verifyNoInteractions(game);
+    }
+    @Test
+    public void stepDoNothing() {
+        List<GUI.OPTION> options = Arrays.asList(GUI.OPTION.UP2, GUI.OPTION.DOWN2, GUI.OPTION.RIGHT2, GUI.OPTION.LEFT2);
+
+        try {
+            saveScoreController.step(game, options, 0);
+        } catch (IOException | UnsupportedAudioFileException | LineUnavailableException e) {
+            fail();
+        }
+        Mockito.verifyNoInteractions(game);
+        Mockito.verifyNoInteractions(saveScore);
+    }
 }
