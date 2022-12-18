e660c88c6c8316524c9c5c9c4101807a24190ab8
Félix Martins <100770338+Minipoloalex@users.noreply.github.com>
2022-12-18 10:57:35 +0000

    Tests for ScoreMenuController

--- src/test/java/ldts/pacman/controller/menu/ScoreMenuControllerTest.java
+++ src/test/java/ldts/pacman/controller/menu/ScoreMenuControllerTest.java
@@ -3,9 +3,13 @@ package ldts.pacman.controller.menu;
 import ldts.pacman.Game;
 import ldts.pacman.gui.GUI;
 import ldts.pacman.application.state.MainMenuState;
+import ldts.pacman.model.menu.ScoreMenu;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class ScoreMenuControllerTest {
     private Game game;
@@ -15,24 +19,26 @@ public class ScoreMenuControllerTest {
         game = Mockito.mock(Game.class);
         scoreMenuController = new ScoreMenuController(null);
     }
-    /*
     @Test
     public void stepNoQuit() {
-        scoreMenuController.step(game, GUI.OPTION.NONE, 0);
-        scoreMenuController.step(game, GUI.OPTION.UP, 0);
-        scoreMenuController.step(game, GUI.OPTION.DOWN, 0);
-        scoreMenuController.step(game, GUI.OPTION.RIGHT, 0);
-        scoreMenuController.step(game, GUI.OPTION.LEFT, 0);
+        List<GUI.OPTION> options = Arrays.asList(GUI.OPTION.UP, GUI.OPTION.DOWN, GUI.OPTION.RIGHT, GUI.OPTION.LEFT,
+                GUI.OPTION.UP2, GUI.OPTION.DOWN2, GUI.OPTION.RIGHT2, GUI.OPTION.LEFT2);
 
+        scoreMenuController.step(game, options, 0);
         Mockito.verifyNoInteractions(game);
     }
     @Test
-    public void stepWithQuit() {
-        scoreMenuController.step(game, GUI.OPTION.SELECT, 0);
-        scoreMenuController.step(game, GUI.OPTION.QUIT, 0);
+    public void stepSelectWithQuit() {
+        scoreMenuController.step(game, List.of(GUI.OPTION.SELECT), 0);
 
-        Mockito.verify(game, Mockito.times(2))
+        Mockito.verify(game, Mockito.times(1))
                 .setState(Mockito.any(MainMenuState.class));
     }
-    */
+    @Test
+    public void stepWithQuit() {
+        scoreMenuController.step(game, List.of(GUI.OPTION.QUIT), 0);
+
+        Mockito.verify(game, Mockito.times(1))
+                .setState(Mockito.any(MainMenuState.class));
+    }
 }
