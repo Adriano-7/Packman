6b5248a34bcc1f6e63e35a526a275aa06a54d617
Félix Martins <100770338+Minipoloalex@users.noreply.github.com>
2022-12-21 11:59:40 +0000

    Removed useless imports
    These were a consequence of the last change

--- src/main/java/ldts/pacman/controller/game/movement/strategy/player/MonsterPlayerStrategy.java
+++ src/main/java/ldts/pacman/controller/game/movement/strategy/player/MonsterPlayerStrategy.java
@@ -1,12 +1,6 @@
 package ldts.pacman.controller.game.movement.strategy.player;
 
 import ldts.pacman.gui.GUI;
-import ldts.pacman.model.game.Position;
-import ldts.pacman.model.game.elements.MovableElement;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class MonsterPlayerStrategy extends PlayerMovementStrategy {
     public MonsterPlayerStrategy() {
--- src/main/java/ldts/pacman/controller/game/movement/strategy/player/PacmanPlayerStrategy.java
+++ src/main/java/ldts/pacman/controller/game/movement/strategy/player/PacmanPlayerStrategy.java
@@ -1,9 +1,6 @@
 package ldts.pacman.controller.game.movement.strategy.player;
 
 import ldts.pacman.gui.GUI;
-import ldts.pacman.model.game.Position;
-import java.util.HashMap;
-import java.util.Map;
 
 public class PacmanPlayerStrategy extends PlayerMovementStrategy {
     public PacmanPlayerStrategy() {
